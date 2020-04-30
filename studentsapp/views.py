36397348a81ca1a4b98b64222ba864517a8d8b0a
suansocute <62133718+suansocute@users.noreply.github.com>
2020-04-30 21:21:26 +0800

    Update views.py

--- studentsapp/views.py
+++ studentsapp/views.py
@@ -26,9 +26,9 @@ parser  = WebhookParser(settings.LINE_CHANNEL_SECRET)
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('Rlf0lUEqOQodKBwGp3DqaT7Opa83/aIEkFyHez0urSALa2e9SAdn6buoABnOmejqhKYvJh4buQKT1PyOhoZGSEJZNK1HIKIxWApAwdnXK8SfcgGsb+U/yqi0WH3EhPDeRm9mUug5+k8WrCxwkwImuwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('528gEzSdPX8Pw9mTJlfpXRpTBdETmaT0LJuHs8+3/qBsiz1lWzBRIfWXQexgOdiPyGxeLtL9HAggCjAp0LBJcqKAdv7h8Ux5JQpKMioyobyYoBQpwer/eA3cA1e89/b80sl219M34lvpXlAAFud++wdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('0e306e90c9ea73b347d04fc0c49e4edf')
+handler = WebhookHandler('43664ae683e1af5eb58087c85b74216d')
 
 line_bot_api.push_message('Ub8e3cf75739079f25a50f82b2cbd4c63', TextSendMessage(text='你可以開始了'))
 
